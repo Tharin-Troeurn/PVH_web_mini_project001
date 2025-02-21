9411d9e7aa5e4b7ebad505d140058d46c6fa6172
Tharin-Troeurn <tharintroeurn@gmail.com>
2025-02-21 22:13:10 +0700

    Finish web miniProject

---
+++ src/scrpt.js
@@ -0,0 +1,22 @@
+let person = {
+    name : "Dara",
+    age : 20
+}
+
+let per1 = {
+    ...person,
+    nationality : "Cambodia",
+    job : "Student"
+}
+console.log(per1)
+
+// let {name, age} = person1;
+
+
+const arr = [1,2,3]
+console.log(arr[0])
+
+function myPromise(){
+    
+}
+
